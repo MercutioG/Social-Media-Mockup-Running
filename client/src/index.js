a1e668ead3c50a1efc2d8651841cbfa663cccfbe
asherpage <110576723+asherpage@users.noreply.github.com>
2023-12-18 14:15:26 -0700

    front end yur

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './Styles/css/main.css';
+import './Styles/main.css';
 
 import Home from './Pages/Home';
 import Login from './Pages/Login';
